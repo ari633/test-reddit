0d01ba5ab0532e9e0b6a38f78a6fa65d0531846a
ari pratomo <ari.pratomo@ajaib.co.id>
2023-12-05 03:20:11 +0000

    Update

--- src/utils/ServerFetcher.js
+++ src/utils/ServerFetcher.js
@@ -1,15 +1,16 @@
-const fetcher = async (params = 'new') => {
+const ACCESS_TOKEN = process.env.REDDIT_ACCESS_TOKEN1;
+const fetcher = async (params = "new") => {
   const res = await fetch(`https://oauth.reddit.com/${params}`, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `bearer ${process.env.REDDIT_ACCESS_TOKEN}`,
+      Authorization: `bearer ${ACCESS_TOKEN}`,
     },
   });
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error("Failed to fetch data");
   }
- 
-  return res.json()
+
+  return res.json();
 };
 
 export default fetcher;
